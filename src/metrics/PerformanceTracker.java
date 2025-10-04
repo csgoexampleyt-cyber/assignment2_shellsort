a900cd6056da8d485901fb7b0ce188c5fba3d224
csgoexampleyt-cyber <csgoexampleyt@gmail.com>
2025-10-05 00:45:11 +0500

    PerformanceTracker.java

--- src/metrics/PerformanceTracker.java
+++ src/metrics/PerformanceTracker.java
@@ -3,16 +3,32 @@ package metrics;
 public class PerformanceTracker {
     private long startTime;
     private long endTime;
+    private long shiftCount;
 
     public void start() {
         startTime = System.nanoTime();
+        shiftCount = 0;
     }
 
     public void stop() {
         endTime = System.nanoTime();
     }
 
+    public void incrementShifts() {
+        shiftCount++;
+    }
+
+    public long getShiftCount() {
+        return shiftCount;
+    }
+
     public double getElapsedMillis() {
         return (endTime - startTime) / 1_000_000.0;
+    }
+
+    public void reset() {
+        startTime = 0;
+        endTime = 0;
+        shiftCount = 0;
     }
 }
